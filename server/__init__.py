14e31a2c1c9881d1ef23b19f50b65292c690e95c
xiaoyao-jimmy <smile.njsx@gmail.com>
2025-08-28 19:50:13 +0800

    Quant business init

---
+++ server/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, 'quant.sqlite')
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+    return app
