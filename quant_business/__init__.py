79fe2f84242966761249b42654aeab83218b016f
xiaoyao-jimmy <46389998+xiaoyao-jimmy@users.noreply.github.com>
2025-09-04 00:27:46 +0800

    Feature Add JWT

--- quant_business/__init__.py
+++ quant_business/__init__.py
@@ -1,24 +1,26 @@
 import os
+
 from flask import Flask
 from flask_jwt_extended import JWTManager
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
+    app.config["JWT_SECRET_KEY"] = '731314806@qq.com'
     app.config.from_mapping(
         DATABASE=os.path.join(app.instance_path, 'quant.sqlite'),
-        JWT_SECRET_KEY=''
+        JWT_SECRET_KEY='731314806@qq.com',
     )
-    jwt = JWTManager(app)
-    
 
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
+    JWTManager(app)
+
     from . import candlestick
-    app.register_blueprint(candlestick.cdbp)
+    app.register_blueprint(candlestick.cdbp, url_prefix='/candlestick')
 
     from . import db
     db.init_app(app)
--- quant_business/candlestick.py
+++ quant_business/candlestick.py
@@ -2,21 +2,22 @@ import json
 
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required
+
 from . import db
 
 cdbp = Blueprint('candlestick', __name__)
 
-@jwt_required()
+
 @cdbp.route('/report_candlestick_sign', methods=['POST'])
+@jwt_required()
 def report_candlestick_sign():
-    # request.authorization().token
-    # request.authorization.token
     rows = [tuple(sign_dict.values()) for sign_dict in request.json]
     db.insert_candlestick(rows)
     return {"status": "ok"}
 
-@jwt_required()
+
 @cdbp.route('/get_candlestick_sign', methods=['GET'])
+@jwt_required()
 def get_candlestick_sign():
     rows = db.select_candlestick_by_date(request.args.get("date"))
-    return {"status": "ok"}
+    return json.dumps([dict(row) for row in rows])
