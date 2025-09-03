bd3e95fa05c23d05e707fb0a254e4da84535b136
xiaoyao-jimmy <smile.njsx@gmail.com>
2025-09-03 18:45:04 +0800

    Feature add JWT & yfinance

--- quant_business/__init__.py
+++ quant_business/__init__.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        DATABASE=os.path.join(app.instance_path, 'quant.sqlite')
+        DATABASE=os.path.join(app.instance_path, 'quant.sqlite'),
+        JWT_SECRET_KEY=''
     )
+    jwt = JWTManager(app)
+    
 
     try:
         os.makedirs(app.instance_path)
--- quant_business/candlestick.py
+++ quant_business/candlestick.py
@@ -1,11 +1,12 @@
 import json
 
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required
 from . import db
 
 cdbp = Blueprint('candlestick', __name__)
 
-
+@jwt_required()
 @cdbp.route('/report_candlestick_sign', methods=['POST'])
 def report_candlestick_sign():
     # request.authorization().token
@@ -14,7 +15,7 @@ def report_candlestick_sign():
     db.insert_candlestick(rows)
     return {"status": "ok"}
 
-
+@jwt_required()
 @cdbp.route('/get_candlestick_sign', methods=['GET'])
 def get_candlestick_sign():
     rows = db.select_candlestick_by_date(request.args.get("date"))
