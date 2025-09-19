7d6a11c0dc7e71c7964421aa3892e0e880d19434
xiaoyao-jimmy <46389998+xiaoyao-jimmy@users.noreply.github.com>
2025-09-20 00:33:16 +0800

    Improve Sql

--- quant_business/schema.sql
+++ quant_business/schema.sql
@@ -1,11 +1,11 @@
--- DROP TABLE IF EXISTS candlestick;
+DROP TABLE IF EXISTS candlestick;
 
---CREATE TABLE IF NOT EXISTS candlestick (
---  symbol TEXT NOT NULL,
---  date TEXT NOT NULL,
---  candlestick_sign TEXT NOT NULL
---);
+CREATE TABLE IF NOT EXISTS candlestick (
+  symbol TEXT NOT NULL,
+  date TEXT NOT NULL,
+  candlestick_sign TEXT,
+  candlestick_bullish_sign TEXT,
+  candlestick_bearish_sign TEXT
+);
 
-ALTER TABLE candlestick ADD COLUMN candlestick_bullish_sign TEXT;
-ALTER TABLE candlestick ADD COLUMN candlestick_bearish_sign TEXT;
 CREATE UNIQUE INDEX idx_candlestick_symbol_date ON candlestick (symbol, date);
